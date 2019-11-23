5118929fadf07421ee204e0915ee6de6bd4be425
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2019-11-23 23:22:43 +0530

    Welcome (index)  page of admin.

---
+++ admin_index.php
@@ -0,0 +1,61 @@
+<!--UPDATE IDEAS-->
+<!-- 1.CUSTOMIZE MOUSE CURSOR-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Admin</title>
+</head>
+<body bgcolor="cyan"> <!-- ENTER BACKGROUND COLOR HERE-->
+<!-- HEADER ELEMENT-->
+<header>
+<div align="center">
+<h1><b><big><i>WELCOME ADMIN!!!</i></big></b></h1>
+</div>
+</header>
+<!--NAVIGATION ELEMENT-->
+<nav>
+<table border="8px" align="right" style="width:100%;" >
+<tr>
+<td align="center" bgcolor="darkkhaki"><a href="Home.php" target="_blank"><b>Home</b></a></td>
+<td align="center" bgcolor="darkseagreen"><a href="info.txt" target="_blank"><b>About us</b></a></td>
+<td align="center" bgcolor="greenyellow"><a href="log_in.php" target="_blank"><b>Log out</b></a></td>
+</tr>
+</nav>
+<!-- ARTICLE ELEMENT-->
+<article class="article">
+<table id="table" align="center" border="10px" style="width:200px;height:500px;border-collapse:collapse" bgcolor="silver">
+<tr>
+<td align="center"><a href="add_dept.php">Add Department</a> </td><!--Add department URL -->
+</tr>
+<tr>
+<td align="center"><a href="add_emp.php">Add Employee </a></td><!-- Add employee URL-->
+</tr>
+<tr>
+<td align="center"><a href="view.php">Show Employee details</a></td><!-- -->
+</tr>
+<tr>
+<td align="center"><a href="modify.php">Modify Record</a></td><!-- -->
+</tr>
+</table>
+</article>
+</body>
+</html>
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
